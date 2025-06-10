3bdd8963bd771c643550ad6e41cb14a4afbdbfeb
biswasshreyan0 <biswasshreyan0@gmail.com>
2025-06-09 20:10:47 -0700

    Update mooyo_mooyo.cpp

--- mooyo_mooyo.cpp
+++ mooyo_mooyo.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 void print_matrix(vector<vector<int>> board, int n) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < 10; j++) {
@@ -11,119 +10,76 @@ void print_matrix(vector<vector<int>> board, int n) {
     }
 }
 
-void flood_fill (vector<vector<int>>& board, const int& i, const int& j, int& size, int& n, int& color) {
-    // cout << "i = " << i << " j = " << j;
-    if (!(i >= 0 and i < n and j >= 0 and j < 10 and board[i][j] == color)) {
+void flood_fill(vector<vector<int>>& board, const int& i, const int& j, int& size, int& n, int& color) {
+    if (!(i >= 0 && i < n && j >= 0 && j < 10 && board[i][j] == color))
         return;
-    }
-    
     size++;
-    board[i][j] = -(color);
+    board[i][j] = -color;
     flood_fill(board, i+1, j, size, n, color);
     flood_fill(board, i-1, j, size, n, color);
     flood_fill(board, i, j+1, size, n, color);
     flood_fill(board, i, j-1, size, n, color);
 }
 
-/*
-void gravity2 (vector<vector<int>>& board, int n) {
-    int cnt = 0;
-
-    //cout << "Initial matrix" << endl;
-    //print_matrix(board, n);
-
+void gravity(vector<vector<int>>& board, int N) {
     for (int j = 0; j < 10; j++) {
-        int dist = 0;
-        
-        for (int i = n-1; i >= 0; i--) {
-            if (board[i][j] == 0) {
-                dist++;
-            } else {
-                swap(board[i+dist][j], board[i][j]);
-            }
-            //cout << "Current matrix" << endl;
-            //print_matrix(board, n);
-        }
-        
-    }
-}
-*/
-void gravity(vector<vector<int>>& board, int N)
-{
-
-    for (int j=0; j<10; j++) {
-        int top = N-1, bottom = N-1;
+        int top = N - 1, bottom = N - 1;
         while (top >= 0) {
             while (top >= 0 && board[top][j] == 0) top--;
             if (top >= 0)
                 board[bottom--][j] = board[top--][j];
         }
         while (bottom >= 0) board[bottom--][j] = 0;
-    }   
+    }
 }
 
 int main() {
-    std::ifstream inputFile("mooyomooyo.in");
-    std::ofstream outputFile("mooyomooyo.out");
-
+    ifstream inputFile("mooyomooyo.in");
+    ofstream outputFile("mooyomooyo.out");
     if (!inputFile.is_open() || !outputFile.is_open()) {
-        std::cerr << "Error opening files." << std::endl;
+        cerr << "Error opening files." << endl;
         return 1;
     }
+    auto* cinBuf = cin.rdbuf(inputFile.rdbuf());
+    auto* coutBuf = cout.rdbuf(outputFile.rdbuf());
 
-    std::streambuf* cinBuffer = std::cin.rdbuf();
-    std::streambuf* coutBuffer = std::cout.rdbuf();
-
-    std::cin.rdbuf(inputFile.rdbuf());
-    std::cout.rdbuf(outputFile.rdbuf());
     int n, k;
     cin >> n >> k;
-
     vector<vector<int>> board(n, vector<int>(10));
     for (int i = 0; i < n; i++) {
         string row;
         cin >> row;
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < 10; j++)
             board[i][j] = row[j] - '0';
-        }
     }
-    /*
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < 10; j++) { 
-            cout << board[i][j] << " ";
-        }
-        cout << endl;
-    }
-    */
+
     bool stop = false;
     while (!stop) {
         stop = true;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < 10; j++) {
                 int color = board[i][j];
-                int size = 0;
                 if (color == 0) continue;
+                int size = 0;
                 flood_fill(board, i, j, size, n, color);
-                for (int i = 0; i < n; i++) {
-                    for (int j = 0; j < 10; j++) {
-                        if (size >= k and board[i][j] == -(color)) {
-                            board[i][j] = 0;
+                for (int x = 0; x < n; x++) {
+                    for (int y = 0; y < 10; y++) {
+                        if (size >= k && board[x][y] == -color) {
+                            board[x][y] = 0;
                             stop = false;
-                        } else if (size < k and board[i][j] == -(color)) {
-                            board[i][j] = color;
+                        } else if (size < k && board[x][y] == -color) {
+                            board[x][y] = color;
                         }
                     }
                 }
-                if (!stop) gravity(board, n);
             }
         }
+        if (!stop) gravity(board, n);
     }
 
     print_matrix(board, n);
-    std::cin.rdbuf(cinBuffer);
-    std::cout.rdbuf(coutBuffer);
 
-    inputFile.close();
-    outputFile.close();
-
+    cin.rdbuf(cinBuf);
+    cout.rdbuf(coutBuf);
+    return 0;
 }
