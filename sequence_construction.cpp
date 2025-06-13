5026de0888157b80977e6b4cfab90ee1923b06ce
biswasshreyan0 <biswasshreyan0@gmail.com>
2025-06-13 10:21:46 -0700

    Update sequence_construction.cpp
    fix edge cases

--- sequence_construction.cpp
+++ sequence_construction.cpp
@@ -1,70 +1,72 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int main() {
-    int t;
-    cin >> t;
+	long long t;
+	cin >> t;
 
-    while (true) {
-        cout << t;
-    }
+	for (long long z = 0; z < t; z++) {
+		long long m, k;
+		cin >> m >> k;
 
-    for (int z = 0; z < t; z++) {
-        int m, k;
-        cin >> m >> k;
+		vector<long long> a;
+		long long a_sum = 0;
+		for (long long bit = 0; bit < 5; bit++) {
+			if (k & (1 << bit)) { // k has a 1 in the bitth position
+				long long cur = (1LL << (1 << bit)) - 1;
+				a.push_back(cur);
+				a_sum += cur;
+			}
+		}
 
-        vector<int> a;
-        int a_sum = 0;
-        for (int bit = 0; bit < 5; bit++) {
-            if (k & (1 << bit)) { // k has a 1 in the bitth position
-                int cur = (1LL << (1 << bit)) - 1;
-                a.push_back(cur);
-                a_sum += cur;
-            }
-        }
+		long long diff = m - a_sum;
+		// cout << "for " << z << " test case, diff = " << diff << endl;
+		vector<long long> b;
+		//bool impossible = false;
 
-        int diff = m - a_sum;
-        vector<int> b;
-        //bool impossible = false;
+		if (diff < 0) {
+			cout << -1 << endl;
+			continue;
+		} else if (diff == 0) {
+			// do nothing
+		} else if (diff >= 2) {
+			if (diff % 2 == 0) {
+				b.push_back(diff/2);
+				b.push_back(diff/2);
+			} else {
+				b.push_back(1);
+				b.push_back(2);
+				b.push_back((diff-3)/2);
+				b.push_back((diff-3)/2);
+			}
 
-        if (diff < 0) {
-            cout << -1 << endl;
-            return;
-        } else if (diff == 0) {
-            // do nothing
-        } else if (diff >= 2) {
-            if (diff % 2 == 0) {
-                b.push_back(diff/2);
-                b.push_back(diff/2);
-            } else {
-                b.push_back(1);
-                b.push_back(2);
-                b.push_back((diff-3)/2);
-                b.push_back((diff-3)/2);
-            }
-        } else if (diff == 1) {
-            bool possible = false;
-            for (int& x : a) {
-                if (x == 1) {
-                    x = 2;
-                    possible = true;
-                    diff -= 1;
-                    b.push_back(diff/2);
-                    b.push_back(diff/2);
-                }
-            }
-            if (!possible) {
-                cout << -1 << endl;
-                return;
-            }
-        }
+		} else if (diff == 1) {
+			bool possible = false;
+			for (long long& x : a) {
+				if (x == 1) {
+					x = 2;
+					possible = true;
+					diff -= 1;
 
-        cout << a.size() + b.size() << endl;
-        for (int x : a) {
-            cout << x << " ";
-        }
-        for (int y : b) {
-            cout << y << " ";
-        }
-    }
+					b.push_back(diff/2);
+					b.push_back(diff/2);
+					break;
+				}
+			}
+			if (!possible) {
+				cout << -1 << endl;
+				continue;
+			}
+		}
+
+		cout << a.size() + b.size() << endl;
+		for (long long x : a) {
+			cout << x << " ";
+		}
+		for (long long y : b) {
+			cout << y << " ";
+		}
+		cout << endl;
+	}
 }
